d30d1f4512643709a7cce997770012e88b465c35
SocketSomeone <socket.someone@gmail.com>
2024-09-17 00:54:18 +0300

    refactor: make more prettier

--- src/necord-lavalink-options.interface.ts
+++ src/necord-lavalink-options.interface.ts
@@ -1,6 +1,5 @@
 import { ManagerOptions } from 'lavalink-client';
 
-// Make sendToShard method optional
 export type NecordLavalinkModuleOptions = Omit<ManagerOptions, 'sendToShard'> & {
-	sendToShard?: (guildId: string, packet: any) => void;
+	sendToShard?: ManagerOptions['sendToShard'];
 };
